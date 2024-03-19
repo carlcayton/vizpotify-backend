f399280337b9e48fd67753213690874c4b7b019e
Arian Carl Cayton <ariancayton@gmail.com>
2024-03-19 22:22:33 +0800

    edit: Change Jackson2JsonRedisSerializer to GenericJackson2JsonRedisSerializer

--- src/main/java/com/arian/vizpotifybackend/config/RedisConfig.java
+++ src/main/java/com/arian/vizpotifybackend/config/RedisConfig.java
@@ -1,12 +1,6 @@
 package com.arian.vizpotifybackend.config;
 
 import com.arian.vizpotifybackend.properties.RedisConfigProperties;
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
-import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,7 +9,6 @@ import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -44,28 +37,7 @@ public class RedisConfig {
         return template;
     }
 
-//    @Bean
-//    public RedisTemplate<String, Object> jsonRedisTemplate() {
-//        RedisTemplate<String, Object> template = new RedisTemplate<>();
-//        template.setConnectionFactory(connectionFactory());
-//
-//        Jackson2JsonRedisSerializer<Object> jsonSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-//
-//        PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator.builder()
-//                .allowIfBaseType(Object.class)
-//                .build();
-//
-//        objectMapper.activateDefaultTyping(ptv, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
-//
-//        jsonSerializer.setObjectMapper(objectMapper);
-//
-//        template.setKeySerializer(new StringRedisSerializer());
-//        template.setValueSerializer(jsonSerializer);
-//        template.afterPropertiesSet();
-//        return template;
-//    }
+
 @Bean
 public RedisTemplate<String, Object> jsonRedisTemplate() {
     RedisTemplate<String, Object> template = new RedisTemplate<>();
