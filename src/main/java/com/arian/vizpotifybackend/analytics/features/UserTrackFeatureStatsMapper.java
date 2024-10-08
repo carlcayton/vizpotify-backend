6ae2d63a26496bcbf8ef0e5d54756ed7da182f81
Arian Carl Cayton <ariancayton@gmail.com>
2024-10-08 20:48:51 +0800

    feat: add UserTrackFeatureStatsMapper interface

---
+++ src/main/java/com/arian/vizpotifybackend/analytics/features/UserTrackFeatureStatsMapper.java
@@ -0,0 +1,14 @@
+package com.arian.vizpotifybackend.analytics.features;
+
+import com.arian.vizpotifybackend.analytics.features.UserTrackFeatureStats;
+import com.arian.vizpotifybackend.analytics.features.UserTrackFeatureStatsDto;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface UserTrackFeatureStatsMapper {
+    UserTrackFeatureStatsDto toDto(UserTrackFeatureStats userTrackFeatureStats);
+    List<UserTrackFeatureStatsDto> toDtoList(List<UserTrackFeatureStats> userTrackFeatureStatsList);
+
+}
